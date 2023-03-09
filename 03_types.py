c59df4d86cc74fbc25de96134d075e8f0a2ad1d3
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-09 00:15:51 -0500

    Added primitive types example

---
+++ 03_types.py
@@ -0,0 +1,28 @@
+# String type
+my_name = "Nicolas"
+# It could be initialize either with single quote o double quote
+my_name = 'Juan'
+print("my_name =>", my_name)
+
+# The type function returns the class type of the argument
+print(type(my_name))
+
+# int type
+my_age = 25
+print("my_age =>", my_age)
+print(type(my_age))
+
+# float type
+my_height = 1.70
+print("my_height =>", my_age)
+print(type(my_height))
+
+# boolean type (True OR False)
+is_single = True
+print("is_single =>", is_single)
+print(type(is_single))
+
+# Example with input
+my_age = input("¿Cuál es tu edad?")
+print("my_age =>", my_age)
+print(type(my_age)) # Input returns a string value
