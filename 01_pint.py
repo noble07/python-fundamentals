913c764f9a42af8b5b93526b4a841c5961436157
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-09 00:02:33 -0500

    Added print examples

---
+++ 01_pint.py
@@ -0,0 +1,23 @@
+# Esto es un comentario
+
+# Print por consola
+print("Hola, esto es python")
+print("Hola soy Juan Camilo y este es mi primer programa en python")
+
+# Operaciones básicas
+print(12+5)
+print(10-5)
+print(2*3)
+print(8/2)
+
+"""
+  Este es un 
+  comentario
+  multilinea
+"""
+
+'''
+  Este también es
+  un comentario
+  multilinea
+'''
