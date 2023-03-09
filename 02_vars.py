1418cc9bb9781dbf09806204ef74eb13eaf4d45a
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-09 00:02:51 -0500

    Added variable declaration examples

---
+++ 02_vars.py
@@ -0,0 +1,20 @@
+print("Hola, vars")
+
+# Declaring a variable and assing a string value to it
+my_name = "Juan"
+print(my_name)
+
+# Assingning a number value to a variable
+my_age = 12
+print(my_age)
+
+# Reassigning a value
+my_name = "Camilo"
+print("aqui cambio", my_name)
+
+# Read a value from the console input
+my_name = input("Igresa tu nombre\n")
+my_age = input("¿Cuantos años tienes")
+
+# f-string
+print(f"{my_name}, tiene {my_age} años")
