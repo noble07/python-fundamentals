06e668fb91015c5c1b74f315da287946150aadd5
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-10 23:23:29 -0500

    Added arithmetic operators example

---
+++ 08_arith.py
@@ -0,0 +1,15 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2) # 5
+print(10 % 2) # 0
+print(10 / 3) # 3.333...
+print(10 % 3) # 1
+# // Only returns the int number of the result
+print(10 // 3) # 3
+print(2**3) # 8
+print(2 ** 3 + 3 - 7 / 1 // 4) # PEMDAS
+print(10/0) # Error
+
+print("Hola " + "mundo")
+print("Hola"*3)
