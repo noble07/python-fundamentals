b2342c30c650be6d7c6eb919cb6ebb4b6065112b
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-10 22:52:19 -0500

    Added number type examples

---
+++ 05_numbers.py
@@ -0,0 +1,31 @@
+# Int values
+lives = 3
+print(type(lives)) # class int
+age = 12
+budget = 100
+
+# Float values
+temperature = 12.12
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 1 # shorthand of lives = lives - 1
+print(lives)
+lives += 5
+print(lives)
+
+number = 35000000000000000000000000000.2
+print(number)
+
+number_b = 0.000000000000000000000000001
+print(number_b)
