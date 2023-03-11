510cf87b4766ff9c6db84f31c7cedc3c6e80ad09
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-10 22:56:02 -0500

    Added boolean type examples

---
+++ 06_booleans.py
@@ -0,0 +1,13 @@
+# Boolean values are Capitalize!
+is_single = True
+print(type(is_single)) # class bool
+is_single = False
+print(is_single)
+
+# not keyword negates the value
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
+print(not is_single)
