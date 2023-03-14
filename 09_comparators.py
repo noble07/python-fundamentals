1146b8730a7d630f6ed9a58255932d7a46a0087d
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-13 19:16:04 -0500

    Added examples of logical comparators

---
+++ 09_comparators.py
@@ -0,0 +1,32 @@
+# > Mayor que
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+
+# < Menor que
+print(3 < 7)
+print(6 < 5)
+print(5 < 5)
+
+# >= Mayor o igual
+print (2 >= 1)
+print(2 >= 2)
+
+# <= Menor o igual
+print(3 <= 6)
+print(3 <= 3)
+
+# Igual a
+print(6 == 6)
+print(5 == 2)
+
+# Diferente de
+print(6 != 2)
+print(2 != 2)
+
+print("Apple" == 'Apple') # True
+print("Apple" == 'apple') # False
+print("1" == 1) # False
+
+age = 15
+print(age >= 18)
